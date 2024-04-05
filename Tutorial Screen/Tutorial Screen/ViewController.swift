e08bfb6a1bfd4fd4b4297774b0fd9c3425a4be83
Naman Malhotra <101506912+NamanMalhotra1706@users.noreply.github.com>
2024-04-05 22:01:50 +0530

    Lab-Tutorial Screen

---
+++ Tutorial Screen/Tutorial Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tutorial Screen
+//
+//  Created by student on 05/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
