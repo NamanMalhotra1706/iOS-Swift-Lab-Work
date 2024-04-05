0dec53e940477b4406e30fa66c5a530082331da6
Naman Malhotra <101506912+NamanMalhotra1706@users.noreply.github.com>
2024-04-05 19:45:41 +0530

    Lab-Rainbow Tabs Class

---
+++ RainbowTabsClass/RainbowTabsClass/OrangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabsClass
+//
+//  Created by student on 04/03/24.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RainbowTabsClass/RainbowTabsClass/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabsClass
+//
+//  Created by student on 04/03/24.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "1"
+    }
+
+
+}
+
+  
