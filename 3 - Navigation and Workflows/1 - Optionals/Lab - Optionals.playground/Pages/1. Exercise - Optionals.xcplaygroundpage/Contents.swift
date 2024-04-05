9b3b45e270fee67fd9db777428f5e8a1fb1b06cc
Naman Malhotra <101506912+NamanMalhotra1706@users.noreply.github.com>
2024-04-05 20:29:34 +0530

    Student 1 - Navigation and Workflows

---
+++ 3 - Navigation and Workflows/1 - Optionals/Lab - Optionals.playground/Pages/1. Exercise - Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+## Exercise - Optionals
+
+ >Throughout the exercises in this playground, you will be printing optional values. The Swift compiler will display a warning: "Expression implicitly coerced from `Int?` to Any." For the purposes of these exercises, you can ignore this warning.
+
+ Imagine you have an app that asks the user to enter his/her age using the keyboard. When your app allows a user to input text, what is captured for you is given as a `String`. However, you want to store this information as an `Int`. Is it possible for the user to make a mistake and for the input to not match the type you want to store?
+
+ Declare a constant `userInputAge` of type `String` and assign it "34e" to simulate a typo while typing age. Then declare a constant `userAge` of type `Int` and set its value using the `Int` initializer that takes an instance of `String` as input. Pass in `userInputAge` as the argument for the initializer. What error do you get?
+ */
+let userInputAge : String = "34";
+let userAge : Int? = Int(userInputAge);
+print("Value of optional type 'Int?' must be unwrapped to a value of type 'Int' ")
+//:  Go back and change the type of `userAge` to `Int?`, and print the value of `userAge`. Why is `userAge`'s value `nil`? Provide your answer in a comment or print statement below.
+print("User age value is \(String(describing: userAge))  its nil beacuse of optional binding we made the type Int to optional(Int) so the value nil is stored to userAge");
+
+/*:
+ Now go back and fix the typo on the value of `userInputAge`. Is there anything about the value printed that seems off?
+
+ Print `userAge` again, but this time unwrap `userAge` using the force unwrap operator.
+ */
+print("User age forcefully binding (!) is : \(userAge!)");
+
+//:  Now use optional binding to unwrap `userAge`. If `userAge` has a value, print it to the console.
+if let userAge = userAge {
+    print("User age has value : \(userAge)");
+}
+
+/*:
+page 1 of 6  |  [Next: App Exercise - Finding a Heart Rate](@next)
+ */
