3a8af4b4f6898c0532f298d95aa03890348baa69
Naman Malhotra <101506912+NamanMalhotra1706@users.noreply.github.com>
2024-04-25 21:41:46 +0530

    Lab - iTunesSearch  (Part 3)

---
+++ Lab-iTunes/6 - WWW Concurrency/lab/iTunesSearch/StoreItemCollectionViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class StoreItemCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ Lab-iTunes/6 - WWW Concurrency/lab/iTunesSearch/StoreItemContainerViewController.swift
@@ -0,0 +1,84 @@
+
+import UIKit
+
+class StoreItemContainerViewController: UIViewController, UISearchResultsUpdating {
+    
+    @IBOutlet var tableContainerView: UIView!
+    @IBOutlet var collectionContainerView: UIView!
+    
+    let searchController = UISearchController()
+    let storeItemController = StoreItemController()
+    
+    var items = [StoreItem]()
+
+    let queryOptions = ["movie", "music", "software", "ebook"]
+    
+    // keep track of async tasks so they can be cancelled if appropriate.
+    var searchTask: Task<Void, Never>? = nil
+    var tableViewImageLoadTasks: [IndexPath: Task<Void, Never>] = [:]
+    var collectionViewImageLoadTasks: [IndexPath: Task<Void, Never>] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.automaticallyShowsSearchResultsController = true
+        searchController.searchBar.showsScopeBar = true
+        searchController.searchBar.scopeButtonTitles = ["Movies", "Music", "Apps", "Books"]
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(fetchMatchingItems), object: nil)
+        perform(#selector(fetchMatchingItems), with: nil, afterDelay: 0.3)
+    }
+                
+    @IBAction func switchContainerView(_ sender: UISegmentedControl) {
+        tableContainerView.isHidden.toggle()
+        collectionContainerView.isHidden.toggle()
+    }
+    
+    @objc func fetchMatchingItems() {
+        
+        self.items = []
+                
+        let searchTerm = searchController.searchBar.text ?? ""
+        let mediaType = queryOptions[searchController.searchBar.selectedScopeButtonIndex]
+        
+        // cancel existing task since we will not use the result
+        searchTask?.cancel()
+        searchTask = Task {
+            if !searchTerm.isEmpty {
+                
+                // set up query dictionary
+                let query = [
+                    "term": searchTerm,
+                    "media": mediaType,
+                    "lang": "en_us",
+                    "limit": "20"
+                ]
+                
+                // use the item controller to fetch items
+                do {
+                    // use the item controller to fetch items
+                    let items = try await storeItemController.fetchItems(matching: query)
+                    if searchTerm == self.searchController.searchBar.text &&
+                          mediaType == queryOptions[searchController.searchBar.selectedScopeButtonIndex] {
+                        self.items = items
+                    }
+                } catch let error as NSError where error.domain == NSURLErrorDomain && error.code == NSURLErrorCancelled {
+                    // ignore cancellation errors
+                } catch {
+                    // otherwise, print an error to the console
+                    print(error)
+                }
+                // apply data source changes
+            } else {
+                // apply data source changes
+            }
+            searchTask = nil
+        }
+    }
+    
+}
